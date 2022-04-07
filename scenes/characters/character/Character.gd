27e389f4190368e815e552fe10fc51ce3d79a832
Colin Barry <colin.andrew.barry@gmail.com>
2022-04-07 00:07:58 -0400

    added Enemy, BasicEnemy, tilemap
    Enemy base scene and BasicEnemy inherited scene. Added a simple tilemap with collision and navigation. BasicEnemy uses navigation to naively follow player.

--- scenes/characters/character/Character.gd
+++ scenes/characters/character/Character.gd
@@ -1,21 +1,21 @@
-extends KinematicBody2D
 class_name Character
-
-var friction: float = 0.15
-var acceleration: int = 40
-var max_speed: int = 100
+extends KinematicBody2D
 
 var move_direction: Vector2 = Vector2.ZERO
 var velocity: Vector2 = Vector2.ZERO
 
+onready var friction: float = 0.15
+onready var acceleration: int = 20
+onready var max_speed: int = 50
+
 
 func _physics_process(_delta: float) -> void:
 	velocity = move_and_slide(velocity)
-	velocity = lerp(velocity, Vector2.ZERO, friction)
+	if move_direction == Vector2.ZERO:
+		velocity = lerp(velocity, Vector2.ZERO, friction)
 
 
 func move() -> void:
-	move_direction = move_direction.normalized()
+	move_direction = move_direction.clamped(1)
 	velocity += move_direction * acceleration
 	velocity = velocity.clamped(max_speed)
-	
