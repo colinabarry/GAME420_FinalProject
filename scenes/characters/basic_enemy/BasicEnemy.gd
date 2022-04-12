b2c727a5fba6854c47e951a78a79c7780d0c86e2
Nolan Stevenson <80541034+RetroPugalicious@users.noreply.github.com>
2022-04-11 21:44:28 -0400

    Finalized Medium Enemy Movement

--- scenes/characters/basic_enemy/BasicEnemy.gd
+++ scenes/characters/basic_enemy/BasicEnemy.gd
@@ -2,5 +2,10 @@ extends Enemy
 
 
 func _physics_process(_delta) -> void:
-	chase()
-	move()
+	pass
+#	Check if move timer is done and the enemy can move
+#	Get the player location vector
+#	Rotate the vector 45 degrees to the left or right, alternating
+#	move to the new spot
+#	chase()
+#	move()
